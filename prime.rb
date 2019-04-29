f4fbbcd25b42b959ed5fa2e7d35131e4cb3f29c4
Joshua Crum <joshua.crum@gmail.com>
2019-04-29 21:16:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -10,3 +10,16 @@ def prime?(number)
       return true
     end
   end
+
+  def prime?(number)
+    start = -1
+    prime_numbers = (start..number).to_a
+    (start..number).each do |i|
+      (start..i).each do |num|
+        if (i % num == 0) && num !=i
+          return false
+        end
+      end
+    end
+    true
+  end
