d9eb4723b0ca1188b86a987e0a7b73d0258bf988
Joshua Crum <joshua.crum@gmail.com>
2019-04-29 21:16:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,7 @@
-def prime?(number)
-  if number < 2
-    return false
-  elsif for i in 2..number-1
-    if number % i == 0
-      return false
-    end
-  end
-    else
-      return true
-    end
-  end
+
 
   def prime?(number)
-    start = -1
+    start = 2
     prime_numbers = (start..number).to_a
     (start..number).each do |i|
       (start..i).each do |num|
