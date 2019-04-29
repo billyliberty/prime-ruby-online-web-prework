34da1653a6b03a0b7c4ce7bc490f68ff08a194dd
Joshua Crum <joshua.crum@gmail.com>
2019-04-29 20:49:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
-
-
-def is_prime? (n)
- for d in 2..(n - 1)
-  if (n % d) == 0
-   return false
+def prime?(number)
+  if number < 2
+    return false
+  elsif for i in 2..number-1
+    if number % i == 0
+      return false
+    end
   end
- end
-
- true
-end
+    else
+      return true
+    end
+  end
