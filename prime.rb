8a7a0f101b0d1620fab4cbaca96c9ad881f50fb5
Joshua Crum <joshua.crum@gmail.com>
2019-04-29 20:37:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
 def prime?(number)
   false if number < 2
     (2...number-1).each do |n|
-    if number % n !== 0
-      true
+    if number % n == 0
+      false
     end
   end
-    false
+    true
   end
-end
 
-end
