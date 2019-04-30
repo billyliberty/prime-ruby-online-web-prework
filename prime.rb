f06f4f5b6d37474372feb207e8d4bf1de3b0a43b
Joshua Crum <joshua.crum@gmail.com>
2019-04-30 00:00:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,9 @@
-
-
-
-    def prime? (number)
-      if number <= 1
-          false
-      elsif number == 2
-          true
-      else
-          (2..number-1).none? { |i| number % i == 0}
-      end
+def prime? (number)
+    if number <= 1
+      false
+    elsif number == 2
+      true
+    else
+      (2..number-1).none? { |i| number % i == 0}
+    end
   end
